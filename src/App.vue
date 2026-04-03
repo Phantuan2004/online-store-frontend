ddc80e4026d8cd6c9fffba0b7ffdbb936d1b7c00
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-04 00:07:46 +0700

    Add breadcrumb to the pages.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Header from "./layouts/user/Header.vue";
 import Footer from "./layouts/user/Footer.vue";
+import Menu from "./components/user/Menu.vue";
+import Breadcrumb from "./components/user/Breadcrumb.vue";
 import { RouterView } from "vue-router";
 </script>
 
 <template>
   <Header />
+  <Menu />
+  <Breadcrumb />
   <router-view />
   <Footer />
 </template>
--- src/components/user/Breadcrumb.vue
+++ src/components/user/Breadcrumb.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- Breadcrumb -->
-  <section class="section-breadcrumb">
+  <section v-if="showBreadcrumb" class="section-breadcrumb">
     <div class="cr-breadcrumb-image">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
             <div class="cr-breadcrumb-title">
-              <h2>Shop</h2>
-              <span><RouterLink to="/">Home</RouterLink> - Shop</span>
+              <h2>{{ breadcrumbTitle }}</h2>
+              <span><RouterLink to="/">Home</RouterLink> - {{ breadcrumbTitle }}</span>
             </div>
           </div>
         </div>
@@ -16,8 +16,39 @@
   </section>
 </template>
 
-<script>
-export default {
-  name: "Breadcrumb",
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const breadcrumbTitles = {
+  About: 'About Us',
+  Shop: 'Shop',
+  Blog: 'Blog',
+  Cart: 'Cart',
+  Checkout: 'Checkout',
+  ContactUs: 'Contact Us',
+  Faq: 'FAQ',
+  Login: 'Login',
+  Register: 'Register',
+  Product: 'Product',
+  Wishlist: 'Wishlist',
+  TrackOrder: 'Track Order',
+  Policy: 'Policy',
+  Terms: 'Terms',
+  ForgotPassword: 'Forgot Password',
 };
+
+const showBreadcrumb = computed(() => route.name !== 'Home');
+
+const breadcrumbTitle = computed(() => {
+  const routeName = typeof route.name === 'string' ? route.name : '';
+
+  if (breadcrumbTitles[routeName]) {
+    return breadcrumbTitles[routeName];
+  }
+
+  return routeName.replace(/([a-z])([A-Z])/g, '$1 $2') || 'Page';
+});
 </script>
