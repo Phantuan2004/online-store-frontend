07b7e8c9b6c7cb7cc2a1e80533092a8661389b5c
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-03 23:21:47 +0700

    Add pagination to shop

--- src/pages/user/Shop.vue
+++ src/pages/user/Shop.vue
@@ -180,6 +180,22 @@
               filters.
             </p>
           </div>
+
+          <nav aria-label="..." class="cr-pagination">
+              <ul class="pagination">
+                  <li class="page-item disabled">
+                      <span class="page-link">Previous</span>
+                  </li>
+                  <li class="page-item active" aria-current="page">
+                      <span class="page-link">1</span>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                      <a class="page-link" href="#">Next</a>
+                  </li>
+              </ul>
+          </nav>
         </div>
       </div>
     </div>
