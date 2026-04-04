d88b13255f10825c60552b620eed40069a75a588
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-05 01:13:26 +0700

    edit loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Loader from "./components/Loader.vue";
+import Loader from "./components/user/Loader.vue";
 import Header from "./layouts/user/Header.vue";
 import Footer from "./layouts/user/Footer.vue";
 import Menu from "./components/user/Menu.vue";
@@ -7,6 +7,7 @@ import Breadcrumb from "./components/user/Breadcrumb.vue";
 import MyCart from "./components/user/MyCart.vue";
 import TabToTop from "./components/user/TabToTop.vue";
 import { RouterView } from "vue-router";
+
 </script>
 
 <template>
