881b10260520d6bd8ea38c622ec1a001c0599900
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-07 23:26:13 +0700

    refactor: extract user layout and simplify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
-import Loader from "./components/user/Loader.vue";
-import Header from "./layouts/user/Header.vue";
-import Footer from "./layouts/user/Footer.vue";
-import Menu from "./components/user/Menu.vue";
-import Breadcrumb from "./components/user/Breadcrumb.vue";
-import MyCart from "./components/user/MyCart.vue";
-import TabToTop from "./components/user/TabToTop.vue";
 import { RouterView } from "vue-router";
-
 </script>
 
 <template>
-  <Loader />
-  <Header />
-  <Menu />
-  <Breadcrumb />
-  <TabToTop />
+  <!-- Đây là entry point chính, nội dung thực tế sẽ được điều phối bởi Layouts thông qua RouterView -->
   <router-view />
-  <MyCart />
-  <Footer />
 </template>
+
+<style>
+/* CSS toàn cục nếu cần */
+</style>
---
+++ src/layouts/user/UserLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Loader from "../../components/user/Loader.vue";
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import Menu from "../../components/user/Menu.vue";
+import Breadcrumb from "../../components/user/Breadcrumb.vue";
+import MyCart from "../../components/user/MyCart.vue";
+import TabToTop from "../../components/user/TabToTop.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <Loader />
+  <Header />
+  <Menu />
+  <Breadcrumb />
+  <TabToTop />
+  <router-view />
+  <MyCart />
+  <Footer />
+</template>
+
+<style scoped>
+</style>
