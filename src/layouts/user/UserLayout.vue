b5fdb2ddb168a5acd298385a7333edc960b2f834
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-09 21:26:47 +0700

    feat: implement user and admin layouts and add vendor script dependencies to index.html

--- src/layouts/user/UserLayout.vue
+++ src/layouts/user/UserLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { onMounted, onUnmounted } from "vue";
 import Loader from "../../components/user/Loader.vue";
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
@@ -8,46 +7,6 @@ import Breadcrumb from "../../components/user/Breadcrumb.vue";
 import MyCart from "../../components/user/MyCart.vue";
 import TabToTop from "../../components/user/TabToTop.vue";
 import { RouterView } from "vue-router";
-import { loadScripts, unloadScripts } from "../../utils/assetLoader";
-
-// Import User CSS
-import "@/assets/user/css/vendor/animate.css";
-import "@/assets/user/css/vendor/aos.min.css";
-import "@/assets/user/css/vendor/bootstrap.min.css";
-import "@/assets/user/css/vendor/flaticons.css";
-import "@/assets/user/css/vendor/jquery.slick.css";
-import "@/assets/user/css/vendor/materialdesignicons.min.css";
-import "@/assets/user/css/vendor/owl.carousel.min.css";
-import "@/assets/user/css/vendor/range-slider.css";
-import "@/assets/user/css/vendor/remixicon.css";
-import "@/assets/user/css/vendor/slick-theme.css";
-import "@/assets/user/css/vendor/swiper-bundle.min.css";
-import "@/assets/user/css/style.css";
-
-const userScripts = [
-  "/src/assets/vendor/js/jquery-3.6.4.min.js",
-  "/src/assets/vendor/js/jquery.zoom.min.js",
-  "/src/assets/vendor/js/bootstrap.bundle.min.js",
-  "/src/assets/vendor/js/mixitup.min.js",
-  "/src/assets/vendor/js/range-slider.js",
-  "/src/assets/vendor/js/aos.min.js",
-  "/src/assets/vendor/js/swiper-bundle.min.js",
-  "/src/assets/vendor/js/slick.min.js",
-  "/src/assets/user/js/main.js",
-];
-
-onMounted(async () => {
-  try {
-    await loadScripts(userScripts);
-    console.log("User scripts loaded successfully");
-  } catch (error) {
-    console.error("Error loading user scripts:", error);
-  }
-});
-
-onUnmounted(() => {
-  unloadScripts(userScripts);
-});
 </script>
 
 <template>
