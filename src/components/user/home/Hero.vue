8497c128c9676b930f31fe053307757aee41e41d
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-15 22:07:57 +0700

    edit banner slide hero

--- src/components/user/home/Hero.vue
+++ src/components/user/home/Hero.vue
@@ -1,26 +1,8 @@
 <template>
   <!-- Hero slider -->
   <section class="section-hero padding-b-100 next">
-    <div class="cr-slider swiper-container" ref="heroSwiper">
+    <div class="cr-slider swiper" ref="heroSwiper">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <div class="cr-hero-banner cr-banner-image-two">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-12">
-                  <div class="cr-left-side-contain slider-animation">
-                    <h5><span>100%</span> Organic Fruits</h5>
-                    <h1>Explore fresh & juicy fruits.</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis beatae consequuntur.</p>
-                    <div class="cr-last-buttons">
-                      <router-link to="/shop" class="cr-button">shop now</router-link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <div class="swiper-slide">
           <div class="cr-hero-banner cr-banner-image-one">
             <div class="container">
@@ -39,8 +21,44 @@
             </div>
           </div>
         </div>
+        <div class="swiper-slide">
+          <div class="cr-hero-banner cr-banner-image-two">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="cr-left-side-contain slider-animation">
+                    <h5><span>100%</span> Organic Fruits</h5>
+                    <h1>Explore fresh &amp; juicy fruits.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis beatae consequuntur.</p>
+                    <div class="cr-last-buttons">
+                      <router-link to="/shop" class="cr-button">shop now</router-link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="cr-hero-banner cr-banner-image-three">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="cr-left-side-contain slider-animation">
+                    <h5><span>100%</span> Organic Vegetables</h5>
+                    <h1>The best way to stuff your wallet.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis beatae consequuntur.</p>
+                    <div class="cr-last-buttons">
+                      <router-link to="/shop" class="cr-button">shop now</router-link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="swiper-pagination"></div>
+      <div class="swiper-pagination" ref="paginationEl"></div>
     </div>
   </section>
 </template>
@@ -54,6 +72,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 const heroSwiper = ref(null);
+const paginationEl = ref(null);
 
 onMounted(() => {
   new Swiper(heroSwiper.value, {
@@ -65,7 +84,7 @@ onMounted(() => {
       disableOnInteraction: false,
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: paginationEl.value,
       clickable: true,
     },
   });
